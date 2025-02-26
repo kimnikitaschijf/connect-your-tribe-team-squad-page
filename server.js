808976ecd2fcad883c0cdd76c560c27746f0c6e6
kimnikitaschijf <kimnikitaschijfhva@icloud.com>
2025-02-26 09:39:47 +0100

    #2 K: Filter  layout toegevoegd
    Ik heb de originele layout van mijn squad page toegevoegd. Deze kan worden toegevoegd aan de base

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const app = express()
 app.use(express.static('public'))
 
 const engine = new Liquid();
-app.engine('liquid', engine.express()); 
+app.engine('liquid', engine.express());
 
 app.set('views', './views')
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', async function (request, response) {
@@ -30,62 +30,62 @@ app.get('/', async function (request, response) {
 })
 
 app.get('/kim', async function (request, response) {
-  const messagesResponse = await fetch(`https://fdnd.directus.app/items/messages/?filter={"for":"Team ${teamName}"}`)
-  const messagesResponseJSON = await messagesResponse.json()
+  const person = await fetch('https://fdnd.directus.app/items/person/?fields=id,name,squads.squad_id.name,fav_color,fav_emoji,fav_country,birthdate,avatar,github_handle,fav_animal,fav_hobby,fav_kitchen&filter[squads][squad_id][name][_eq]=1G&filter[birthdate][_neq]=null&sort=birthdate')
+  const personResponseJSON = await person.json()
 
   response.render('kim.liquid', {
-    teamName: teamName,
-    messages: messagesResponseJSON.data
+    persons: personResponseJSON.data,
+    show: "avatar",
   })
-})
-
-app.get('/akiko', async function (request, response) {
-  const messagesResponse = await fetch(`https://fdnd.directus.app/items/messages/?filter={"for":"Team ${teamName}"}`)
-  const messagesResponseJSON = await messagesResponse.json()
-
-  response.render('akiko.liquid', {
-    teamName: teamName,
-    messages: messagesResponseJSON.data
   })
-})
 
-app.get('/viresh', async function (request, response) {
-  const person = await fetch('https://fdnd.directus.app/items/person/?fields=name,squads.squad_id.name,github_handle,fav_color&filter={"github_handle":{"_neq":"null"}}')
-  const personResponseJSON = await person.json()
+  app.get('/akiko', async function (request, response) {
+    const messagesResponse = await fetch(`https://fdnd.directus.app/items/messages/?filter={"for":"Team ${teamName}"}`)
+    const messagesResponseJSON = await messagesResponse.json()
 
-  response.render('viresh.liquid', {persons: personResponseJSON.data})
-})
-
-app.get('/ties', async function (request, response) {
-  const person = await fetch('https://fdnd.directus.app/items/person/?fields=name,squads.squad_id.name,github_handle,fav_color&filter={"github_handle":{"_neq":"null"}}')
-  const personResponseJSON = await person.json()
-
-  response.render('ties.liquid', {persons: personResponseJSON.data})
-})
-
-app.post('/', async function (request, response) {
-  await fetch('https://fdnd.directus.app/items/messages/', {
-    method: 'POST',
-    body: JSON.stringify({
-      for: `Team ${teamName}`,
-      from: request.body.from,
-      text: request.body.text
-    }),
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8'
-    }
-  });
-
-  response.redirect(303, '/')
-})
-
-
-app.set('port', process.env.PORT || 8001)
-
-if (teamName == '') {
-  console.log('Voeg eerst de naam van jullie team in de code toe.')
-} else {
-  app.listen(app.get('port'), function () {
-    console.log(`Application started on http://localhost:${app.get('port')}`)
+    response.render('akiko.liquid', {
+      teamName: teamName,
+      messages: messagesResponseJSON.data
+    })
   })
-}
+
+  app.get('/viresh', async function (request, response) {
+    const person = await fetch('https://fdnd.directus.app/items/person/?fields=name,squads.squad_id.name,github_handle,fav_color&filter={"github_handle":{"_neq":"null"}}')
+    const personResponseJSON = await person.json()
+
+    response.render('viresh.liquid', { persons: personResponseJSON.data })
+  })
+
+  app.get('/ties', async function (request, response) {
+    const person = await fetch('https://fdnd.directus.app/items/person/?fields=name,squads.squad_id.name,github_handle,fav_color&filter={"github_handle":{"_neq":"null"}}')
+    const personResponseJSON = await person.json()
+
+    response.render('ties.liquid', { persons: personResponseJSON.data })
+  })
+
+  app.post('/', async function (request, response) {
+    await fetch('https://fdnd.directus.app/items/messages/', {
+      method: 'POST',
+      body: JSON.stringify({
+        for: `Team ${teamName}`,
+        from: request.body.from,
+        text: request.body.text
+      }),
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8'
+      }
+    });
+
+    response.redirect(303, '/')
+  })
+
+
+  app.set('port', process.env.PORT || 8001)
+
+  if (teamName == '') {
+    console.log('Voeg eerst de naam van jullie team in de code toe.')
+  } else {
+    app.listen(app.get('port'), function () {
+      console.log(`Application started on http://localhost:${app.get('port')}`)
+    })
+  }
